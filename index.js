2a36782692d0d52338103318c2f3a12da1037512
Johansen <Johamaster@gmail.com>
2024-05-15 16:05:51 -0600

    Añadimos funcionalidad LocalStorage

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 var peliculas = [];
 
-function agregarPelicula() {
-  var peliculaUrl = document.getElementById("pelicula").value;
+document.addEventListener("DOMContentLoaded", () => {
+  peliculas = JSON.parse(localStorage.getItem("peliculas")) || [];
+
+  // Limpia la lista antes de volver a agregar las películas
   var listaPelicula = document.getElementById("listaPeliculas");
-  var urlRegex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+  listaPelicula.innerHTML = "";
 
-  if (peliculaUrl !== "" && peliculaUrl.match(urlRegex)) {
-    peliculas.push(peliculaUrl);
-    var index = peliculas.length - 1;
-
-    var contenedorPeliculas = document.createElement("div")
-    contenedorPeliculas.setAttribute("class", "box_pelicula")
+  peliculas.forEach(function (peliculaUrl, index) {
+    var contenedorPeliculas = document.createElement("div");
+    contenedorPeliculas.setAttribute("class", "box_pelicula");
 
     var nuevaPelicula = document.createElement("img");
     nuevaPelicula.src = peliculaUrl;
@@ -24,12 +23,46 @@ function agregarPelicula() {
 
     contenedorPeliculas.appendChild(nuevaPelicula);
     contenedorPeliculas.appendChild(botonEliminar);
-    listaPelicula.appendChild(contenedorPeliculas)
+    listaPelicula.appendChild(contenedorPeliculas);
+  });
+});
+
+function agregarPelicula() {
+  var peliculaUrl = document.getElementById("pelicula").value;
+  var listaPelicula = document.getElementById("listaPeliculas");
+  var urlRegex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+
+  if (peliculaUrl !== "" && peliculaUrl.match(urlRegex)) {
+    peliculas.push(peliculaUrl);
+    var index = peliculas.length - 1;
+
+    var contenedorPeliculas = document.createElement("div");
+    contenedorPeliculas.setAttribute("class", "box_pelicula");
+
+    var nuevaPelicula = document.createElement("img");
+    nuevaPelicula.src = peliculaUrl;
+
+    var botonEliminar = document.createElement("button");
+    botonEliminar.innerText = "Eliminar";
+
+    botonEliminar.addEventListener("click", function () {
+      eliminarPelicula(index);
+    });
+
+    contenedorPeliculas.appendChild(nuevaPelicula);
+    contenedorPeliculas.appendChild(botonEliminar);
+
+    listaPelicula.appendChild(contenedorPeliculas);
 
     document.getElementById("pelicula").value = "";
   } else {
     alert("Debes Escribir una URL valida");
   }
+  sincronizarStorage();
+}
+
+function sincronizarStorage() {
+  localStorage.setItem("peliculas", JSON.stringify(peliculas));
 }
 
 function eliminarPelicula(index) {
@@ -40,10 +73,10 @@ function eliminarPelicula(index) {
   var listaPelicula = document.getElementById("listaPeliculas");
   listaPelicula.innerHTML = "";
 
-  // Vuelve a agregar todas las películas
+  // Vuelve a agregar todas las películas excepto la eliminada
   peliculas.forEach(function (peliculaUrl, index) {
-    var contenedorPeliculas = document.createElement("div")
-    contenedorPeliculas.setAttribute("class", "box_pelicula")
+    var contenedorPeliculas = document.createElement("div");
+    contenedorPeliculas.setAttribute("class", "box_pelicula");
 
     var nuevaPelicula = document.createElement("img");
     nuevaPelicula.src = peliculaUrl;
@@ -57,8 +90,9 @@ function eliminarPelicula(index) {
 
     contenedorPeliculas.appendChild(nuevaPelicula);
     contenedorPeliculas.appendChild(botonEliminar);
-    listaPelicula.appendChild(contenedorPeliculas)
+    listaPelicula.appendChild(contenedorPeliculas);
   });
-}
 
-console.log(peliculas)
+  // Sincroniza el localStorage después de realizar todas las operaciones
+  sincronizarStorage();
+}
